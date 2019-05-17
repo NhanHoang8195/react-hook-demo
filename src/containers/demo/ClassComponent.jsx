4f97318dba0e7b98c70499d858e6afe9dbf6309a
NhanHoang8195 <trongnhan8195@gmail.com>
2019-05-17 16:48:01 +0700

    Updated for presentation

--- src/containers/demo/ClassComponent.jsx
+++ src/containers/demo/ClassComponent.jsx
@@ -1,15 +1,63 @@
 import React, { Component } from 'react';
+import { LayoutContext, LanguageContext } from '../../context';
 
 export default class ClassComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'Nhan',
+            surname: 'Hoang',
+            width: window.innerWidth,
+        };
+        this.handleChangeName= this.handleChangeName.bind(this);
+        this.handleChangeSurname= this.handleChangeSurname.bind(this);
+        this.handleChangeWidth= this.handleChangeWidth.bind(this);
+    }
+    componentDidMount() {
+        document.title = this.state.surname + ' ' + this.state.name;
+        window.addEventListener('resize', this.handleChangeWidth);
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        document.title = this.state.surname + ' ' + this.state.name;
+    }
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleChangeWidth);
+    }
+
+    handleChangeName(e) {
+        this.setState(({name: e.target.value}));
+    }
+    handleChangeSurname(e) {
+        this.setState(({surname: e.target.value}));
+    }
+    handleChangeWidth() {
+        this.setState({width: window.innerWidth});
+    }
     render() {
         return (
-                <div>
-                    <form>
-                        <p>Here is class component</p>
-                        <label>Name:</label>
-                        <input value={this.props.name} readOnly />
-                    </form>
-                </div>
+            <LayoutContext.Consumer>
+                {theme => (
+                    <div className={theme}>
+                        <form>
+                            <p>Here is class component</p>
+                            <label>Name:</label>
+                            <input value={this.state.name} onChange={this.handleChangeName} />
+                            <label>Surame:</label>
+                            <input value={this.state.surname} onChange={this.handleChangeSurname} />
+                            <LanguageContext.Consumer>
+                                {language => (
+                                    <>
+                                        <label>Language:</label>
+                                        <input value={language} readOnly />
+                                    </>
+                                )}
+                            </LanguageContext.Consumer>
+                            <label>Width:</label>
+                            <input value={this.state.width} readOnly />
+                        </form>
+                    </div>
+                )}
+            </LayoutContext.Consumer>
         );
-    }
+    }1
 }
--- src/containers/demo/HookComponent.jsx
+++ src/containers/demo/HookComponent.jsx
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { LayoutContext, LanguageContext } from '../../context';
 
 export function HookComponent(props) {
+    const theme = useContext(LayoutContext);
+    const language = useContext(LanguageContext);
+    const name = useFormInput('Nhan');
+    const surname = useFormInput('Hoang');
+    const width = useWindowWidth();
+    useTitle(surname.value + ' ' + name.value);
     return (
-        <div>
+        <div className={theme}>
             <form>
                 <p>Here is Hook component</p>
                 <label>Name:</label>
-                <input value={props.name} readOnly />
+                <input {...name} />
+                <label>Surame:</label>
+                <input {...surname} />
+                <label>Language:</label>
+                <input value={language} readOnly />
+                <label>Width:</label>
+                <input value={width} readOnly />
             </form>
         </div>
     );
+}
+function useTitle(title) {
+    useEffect(() => {
+        document.title = title;
+    });
+}
+function useFormInput(initialValue) {
+    const [value, setValue] = useState(initialValue);
+    function handleChangeValue(e) {
+        setValue(e.target.value);
+    }
+    return {
+        value,
+        onChange: handleChangeValue
+    }
+}
+
+function useWindowWidth() {
+    const [width, setWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        function handleChangeWidth() {
+            setWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', handleChangeWidth);
+        return () => {
+            window.removeEventListener('resize', handleChangeWidth);
+        }
+    });
+    return width;
 }
