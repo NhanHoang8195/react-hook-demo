abce9d00949d20e084faad00402af170a600e064
hoang nhan <trongnhan8195@gmail.com>
2019-05-17 10:09:53 +0700

    update code

--- src/containers/demo/ClassComponent.jsx
+++ src/containers/demo/ClassComponent.jsx
@@ -1,59 +1,15 @@
 import React, { Component } from 'react';
-import { LanguageContext, LayoutContext } from '../../context';
 
 export default class ClassComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: 'Nhan',
-            surname: 'Hoang',
-            width: window.innerWidth,
-        };
-        this.handleChangeName = this.handleChangeName.bind(this);
-        this.handleChangeSurname = this.handleChangeSurname.bind(this);
-        this.handleChangeWindowSize = this.handleChangeWindowSize.bind(this);
-    }
-    componentDidMount() {
-        document.title = this.state.surname + ' ' + this.state.name;
-        window.addEventListener('resize', this.handleChangeWindowSize);
-    }
-    componentDidUpdate() {
-        document.title = this.state.surname + ' ' + this.state.name;
-    }
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.handleChangeWindowSize);
-    }
-
-    handleChangeName(e) {
-        this.setState({name: e.target.value});
-    }
-    handleChangeSurname(e) {
-        this.setState({surname: e.target.value});
-    }
-    handleChangeWindowSize() {
-        this.setState({width: window.innerWidth});
-    }
     render() {
         return (
-            <LayoutContext.Consumer>
-                {theme => (<div className={theme}>
+                <div>
                     <form>
                         <p>Here is class component</p>
                         <label>Name:</label>
-                        <input value={this.state.name} onChange={this.handleChangeName} />
-                        <label>Surname:</label>
-                        <input value={this.state.surname} onChange={this.handleChangeSurname} />
-                        <LanguageContext.Consumer>
-                            {language => (<>
-                                <label>Language:</label>
-                                <input value={language} readOnly />
-                            </>)}
-                        </LanguageContext.Consumer>
-                        <label>Width:</label>
-                        <input value={this.state.width} readOnly />
+                        <input value={this.props.name} readOnly />
                     </form>
-                </div>)}
-            </LayoutContext.Consumer>
+                </div>
         );
     }
 }
--- src/containers/demo/HookComponent.jsx
+++ src/containers/demo/HookComponent.jsx
@@ -1,54 +1,13 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { LanguageContext, LayoutContext } from '../../context';
+import React from 'react';
 
 export function HookComponent(props) {
-    const theme = useContext(LayoutContext);
-    const language = useContext(LanguageContext);
-    const name = useFormInput('Nhan');
-    const surname = useFormInput('Hoang');
-    const windowWidth = useWindowWidth();
-    // useTitle(surname.value + ' ' + name.value);
-    document.title = surname.value + ' ' + name.value;
     return (
-        <div className={theme}>
+        <div>
             <form>
-                <p>Here is function component</p>
+                <p>Here is Hook component</p>
                 <label>Name:</label>
-                <input {...name} />
-                <label>Surname:</label>
-                <input {...surname} />
-                <label>Language:</label>
-                <input value={language} readOnly />
-                <label>Width:</label>
-                <input value={windowWidth} readOnly />
+                <input value={props.name} readOnly />
             </form>
         </div>
     );
-}
-function useTitle(title) {
-    document.title = title;
-}
-function useWindowWidth() {
-    const [width, setWidth] = useState(window.innerWidth);
-    useEffect(() => {
-        function handleChangeWindowSize() {
-            setWidth(window.innerWidth);
-        }
-        window.addEventListener('resize', handleChangeWindowSize);
-        return () => {
-            window.removeEventListener('resize', handleChangeWindowSize);
-        }
-    });
-    return width;
-}
-
-function useFormInput(initialValue) {
-    const [value, setValue] = useState(initialValue);
-    function handleChangeName(e) {
-        setValue(e.target.value);
-    }
-    return {
-      value,
-      onChange: handleChangeName,
-    };
 }
